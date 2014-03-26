b05af587b2ea702fe3e28e946e3ee3ba5b444986
Scott Kolodziej <spkolodziej@gmail.com>
2014-03-25 21:06:36 -0500

    Add tax calculator solution

---
+++ Numbers/tax.go
@@ -0,0 +1,38 @@
+// Tax Calculator - Asks the user to enter a cost and either a country or
+// state tax. It then returns the tax plus the total cost with tax.
+
+package main
+
+import "fmt"
+
+func main() {
+  var p float64
+  var t float64
+  fmt.Println("What is the base price of the purchase? ($)")
+  _, err := fmt.Scanf("%f", &p)
+
+  if err != nil {
+    fmt.Println("Error:", err)
+    return
+  }
+
+  fmt.Println("What is the tax rate? (%)")
+  _, err = fmt.Scanf("%f", &t)
+
+  if err != nil {
+    fmt.Println("Error:", err)
+    return
+  }
+
+  fmt.Printf("A purchase of $%.2f with a tax rate of %.2f%%\n", p, t)
+  totalPrice, tax := CalculateTax(p, t/100)
+  fmt.Printf("Total Tax: $%.2f\n", tax)
+  fmt.Printf("Total Purchase Price: $%.2f\n", totalPrice)
+}
+
+func CalculateTax(price float64, taxRate float64) (totalPrice float64, tax float64) {
+  tax = price*taxRate
+  totalPrice = price+tax
+
+  return totalPrice, tax
+}
--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Numbers
 
 [**Credit Card Validator**](https://github.com/ScottKolo/GoProjects/blob/master/Numbers/creditcard.go) - Takes in a credit card number from a common credit card vendor (Visa, MasterCard, American Express, Discoverer) and validates it to make sure that it is a valid number (look into how credit cards use a checksum).
 
-**Tax Calculator** - Asks the user to enter a cost and either a country or state tax. It then returns the tax plus the total cost with tax.
+[**Tax Calculator**](https://github.com/ScottKolo/GoProjects/blob/master/Numbers/tax.go) - Asks the user to enter a cost and either a country or state tax. It then returns the tax plus the total cost with tax.
 
 **Factorial Finder** - The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. Solve this using both loops and recursion.
 
