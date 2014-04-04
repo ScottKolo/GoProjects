164c0ca648c52c35eaf7077ac1788192a3c72fd1
Scott Kolodziej <spkolodziej@gmail.com>
2014-04-03 19:58:14 -0500

    go fmt changes

--- Numbers/factorial.go
+++ Numbers/factorial.go
@@ -5,45 +5,45 @@
 package main
 
 import (
-  "fmt"
-  )
+	"fmt"
+)
 
 func main() {
-  var n int
+	var n int
 
-  fmt.Println("Compute factorial for what number?")
-  _, err := fmt.Scanf("%d", &n)
-  if err != nil {
-    fmt.Println("Error:", err)
-    return
-  }
-  if n > 25 {
-    fmt.Println("Error:", n, "is too big! Try an integer in [1, 25].")
-    return
-  }
-  if n < 0 {
-    fmt.Println("Error:", n, "is too small! Try an integer in [1, 25].")
-    return
-  }
+	fmt.Println("Compute factorial for what number?")
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	if n > 25 {
+		fmt.Println("Error:", n, "is too big! Try an integer in [1, 25].")
+		return
+	}
+	if n < 0 {
+		fmt.Println("Error:", n, "is too small! Try an integer in [1, 25].")
+		return
+	}
 
-  fmt.Printf("Loop implementation:      %d! = %d\n", n, factorialLoop(n))
-  fmt.Printf("Recursive implementation: %d! = %d\n", n, factorialRecursive(n))
+	fmt.Printf("Loop implementation:      %d! = %d\n", n, factorialLoop(n))
+	fmt.Printf("Recursive implementation: %d! = %d\n", n, factorialRecursive(n))
 }
 
 func factorialLoop(n int) int {
-  if n == 0 {
-    return 1
-  }
-  result := n
-  for i := n-1; i > 1; i-- {
-    result *= i
-  }
-  return result
+	if n == 0 {
+		return 1
+	}
+	result := n
+	for i := n - 1; i > 1; i-- {
+		result *= i
+	}
+	return result
 }
 
 func factorialRecursive(n int) int {
-  if n == 0 {
-    return 1
-  }
-  return n*factorialRecursive(n-1)
+	if n == 0 {
+		return 1
+	}
+	return n * factorialRecursive(n-1)
 }
--- Web/time.go
+++ Web/time.go
@@ -4,17 +4,17 @@
 package main
 
 import (
-  "github.com/beevik/ntp" // Using pre-made NTP protocol implementation
-  "fmt"
-  )
+	"fmt"
+	"github.com/beevik/ntp" // Using pre-made NTP protocol implementation
+)
 
 func main() {
-  time, err := ntp.Time("time.nist.gov")
-  if err != nil {
-    fmt.Println("Error:", err)
-    return
-  }
-  const layout = "3:04:05 PM (MST) on Monday, January _2, 2006"
-  fmt.Println("Current Local Time:")
-  fmt.Println(time.Local().Format(layout))
+	time, err := ntp.Time("time.nist.gov")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	const layout = "3:04:05 PM (MST) on Monday, January _2, 2006"
+	fmt.Println("Current Local Time:")
+	fmt.Println(time.Local().Format(layout))
 }
