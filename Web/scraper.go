38e87e1ca18eac759e3d07a62ce3663235cf4a8f
Scott Kolodziej <spkolodziej@gmail.com>
2014-04-09 20:20:31 -0500

    Initial commit of web page scraper

---
+++ Web/scraper.go
@@ -0,0 +1,15 @@
+// Create an application which connects to a site and pulls out all links, or
+// images, and saves them to a list. *Optional: Organize the indexed content
+// and don’t allow duplicates. Have it put the results into an easily
+// searchable index file.*
+
+package main
+
+import (
+  "fmt"
+  "io"
+  )
+
+func main() {
+  
+}
